88fd09603fc36b0978ee7991e530722a82dae26d
Robert Godlewski <robtechg5@gmail.com>
2022-05-20 19:09:31 -0700

    Day 2 Recursion proactice.

--- recursion/recursion_todo1.js
+++ recursion/recursion_todo1.js
@@ -3,22 +3,14 @@ console.log('Recursion Todo 1');
 
 
 // Recursive Sigma - returns the sum of 1 to the given number
-function rSigma(num, sum=0) {
-    // Turning num into an integer
+function rSigma(num) {
     num = Math.trunc(num);
-    if (num < 1) {
-        // Sigma anything lower than 0 is 0
+    if (num <= 0) {
         return 0;
+    } else if (num == 1) {
+        return 1;
     } else {
-        if (num == 1) {
-            // Initialising the sum
-            sum += 1;
-        } else {
-            // Incrementing the sum and adding num to it
-            sum += rSigma(num-1, sum);
-            sum += num;
-        }
-        return sum;
+        return rSigma(num-1) + num;
     }
 }
 
@@ -31,21 +23,12 @@ console.log(`The rSigma(-1) = ${rSigma(-1)}`);
 
 
 // Recursive Factorial - returns the product of ints from 1 to num
-function rFact(num, ans=0) {
-    // Make sure that num is an int not a float
+function rFact(num) {
     num = Math.trunc(num);
-    if (num <= 0) {
+    if (num <= 1) {
         return 1;
     } else {
-        if (num == 1) {
-            ans = 1;
-        } else {
-            // Set the ans to the new factorial number
-            ans = rFact(num-1, ans);
-            // increment ans with num
-            ans *= num;
-        }
-        return ans;
+        return rFact(num-1) * num;
     }
 }
 
@@ -55,28 +38,3 @@ console.log(`The rFact(0) = ${rFact(0)}`);
 console.log(`The rFact(3) = ${rFact(3)}`);
 // expected = 720
 console.log(`The rFact(6.5) = ${rFact(6.5)}`);
-
-
-/*
-function floodFill(canvas2D, startXY, newColor, oldColor=0) {
-    if (oldColor == 0) {
-        oldColor = canvas2D[startXY[0]][startXY[1]];
-    }
-
-}
-
-// example canvas2D
-arr = [
-    [3,2,3,4,3],
-    [2,3,3,4,0],
-    [7,3,3,5,3],
-    [6,5,3,4,1],
-    [1,2,3,3,3]
-];
-console.log('canvas2D Start:');
-console.log('[');
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-}
-console.log(']');
-*/
---
+++ recursion/recursion_todo2.js
@@ -0,0 +1,38 @@
+console.log('###############################');
+console.log('Recursion Todo 2');
+
+
+// Recursive Fibonacci
+function rFib(num) {
+    num = Math.trunc(num);
+    if (num <= 0) {
+        return 0;
+    } else if (num == 1) {
+        return 1;
+    } else {
+        return rFib(num-1) + rFib(num-2);
+    }
+}
+
+// expected 5
+console.log(`The rFib(5) = ${rFib(5)}`);
+// expected 34
+console.log(`The rFib(9) = ${rFib(9)}`);
+
+
+// Recursive Tribonacci
+function rTrib(num) {
+    num = Math.trunc(num);
+    if (num <= 0) {
+        return 0;
+    } else if (num == 1) {
+        return 1;
+    } else {
+        return rTrib(num-1) + rTrib(num-2) + rTrib(num-3);
+    }
+}
+
+// expected 2
+console.log(`The rTrib(3) = ${rTrib(3)}`);
+// expected 13
+console.log(`The rTrib(6) = ${rTrib(6)}`)
