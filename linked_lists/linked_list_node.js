ad027b91bb029a27e53c361ad7acc5a137e88cd3
Robert Godlewski <robtechg5@gmail.com>
2022-06-14 12:04:47 -0700

    Added comments to the Linked List Node Class.

--- linked_lists/linked_list_node.js
+++ linked_lists/linked_list_node.js
@@ -1,8 +1,11 @@
-// Basic Node that we will need to store data
+// Basic Linked List Node that we will need to store data
 class Node {
     constructor(data) {
+        // Values are stored in the data
         this.data = data;
+        // Reference point to the next node that comes next in the list
         this.next = null;
+        // Reference point to the previous node that came before in the list
         this.previous = null;
     }
 }
