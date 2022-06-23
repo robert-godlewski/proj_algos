36f61dabac8080f2416d1789e0d87770f442ad54
Robert Godlewski <robtechg5@gmail.com>
2022-06-23 11:15:33 -0700

    Optimized solution for Bubble Sort with arrays.

--- sorts/bubble_selection_sorts.js
+++ sorts/bubble_selection_sorts.js
@@ -28,19 +28,44 @@ var PrintLL = (ll) => {
 console.log('---------------');
 console.log('Bubble Sorting an array.')
 // Basic Bubble Sorts sort the array in place with multiple passes via a nested loop. o(n^2) solution.
+/*
 var BubbleSortArr = (arr) => {
     for (var k=1; k < arr.length; k++) {
-        for (var i=0; i < arr.length-k; i++) {
+        for (var i=0; i < arr.length; i++) {
             if (arr[i] > arr[i+1]) {
                 var temp = arr[i];
                 arr[i] = arr[i+1];
                 arr[i+1] = temp;
             }
             // arr after every check
-            console.log(arr);
+            //console.log(arr);
         }
         // arr at each pass
-        //console.log(arr);
+        console.log(arr);
+    }
+};
+*/
+
+// Optimized solution of a Bubble Sort algorithm for arrays
+var BubbleSortArr = (arr) => {
+    var len = arr.length;
+    for (var k=0; k < len; k++) {
+        var swapped = false;
+        for (var i=0; i < len-k-1; i++) {
+            if (arr[i] > arr[i+1]) {
+                var temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+                swapped = true;
+            }
+            // arr after every check
+            //console.log(arr);
+        }
+        // arr at each pass
+        console.log(arr);
+        if (swapped == false) {
+            break;
+        }
     }
 };
 
@@ -55,33 +80,11 @@ console.log('---------------');
 console.log('Bubble Sorting a Linked List.');
 
 // Basic Bubble sort for a linked list - Not solved - infinate loop problem
-/*var BubbleSortLL = (ll) => {
-    var is_sorting = true;
-    var high_val = ll.head.data;
-    while (is_sorting) {
-        var current_node = ll.head;
-        while (current_node) {
-            if (current_node.next) {
-                if (current_node.data > current_node.next.data) {
-                    var temp = current_node.data;
-                    current_node.data = current_node.next.data;
-                    current_node.next.data = temp;
-                }
-            } else if (current_node.data >= high_val) {
-                if (current_node.data > high_val) {
-                    high_val = current_node.data;
-                }
-                current_node = ll.head;
-            } else {
-                is_sorting = false;
-            }
-            current_node = current_node.next;
-        }
-    }
-};*/
+/*
+var BubbleSortLL = (ll) => {};
 
 // Testing BubbleSortLL
-/*var ll = new LL();
+var ll = new LL();
 var node1 = new Node(5);
 var node2 = new Node(2);
 var node3 = new Node(4);
@@ -96,4 +99,5 @@ console.log('Original Linked List:');
 PrintLL(ll);
 console.log('Sorted Linked List:');
 BubbleSortLL(ll);
-PrintLL(ll);*/
+PrintLL(ll);
+*/
