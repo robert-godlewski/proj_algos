d87f2793d801269b7c82dd7ef924a0bd3c081fb9
Robert Godlewski <robtechg5@gmail.com>
2022-07-07 16:58:07 -0700

    Soved a merge sort array and an interleave array algorithms.

---
+++ sets_heaps/sets1.js
@@ -0,0 +1,62 @@
+console.log('###############################');
+console.log('merge and partition sorting');
+
+
+console.log('---------------');
+console.log('Interleaving Arrays.');
+// Interleaves an unsorted array into one array - O(n) solution, took me 15 min to solve
+var InterleaveArr = (arr1, arr2) => {
+    var arr = [];
+    //console.log(`arr1 = [${arr1}]`);
+    //console.log(`arr2 = [${arr2}]`);
+    var i = 0;
+    while (i < arr1.length || i < arr2.length) {
+        if (i < arr1.length) {
+            arr.push(arr1[i]);
+        }
+        if (i < arr2.length) {
+            arr.push(arr2[i]);
+        }
+        i++;
+    }
+    return arr;
+}
+
+// Testing InterleaveArr
+var arr = InterleaveArr([77,22,11,22], [2,6,7,2,6,2]);
+console.log(`Interleaved array = [${arr}]`);
+
+
+console.log('---------------');
+console.log('Merge Sorted Arrays.');
+// Merges and sorts 2 sorted arrays from lowest to highest - O(n), took about 15 min to solve
+var MergeSortArr = (arr1, arr2) => {
+    var arr = [];
+    //console.log(`arr1 = [${arr1}]`);
+    //console.log(`arr2 = [${arr2}]`);
+    var i = 0;
+    var j = 0;
+    while (i < arr1.length || j < arr2.length) {
+        if (i < arr1.length && j < arr2.length) {//True
+            if (arr1[i] <= arr2[j]) {//True
+                arr.push(arr1[i]);
+                i++;
+            } else {
+                // arr1[i] > arr2[j]
+                arr.push(arr2[j]);
+                j++;
+            }
+        } else if (i < arr1.length) {
+            arr.push(arr1[i]);
+            i++;
+        } else if (j < arr2.length) {
+            arr.push(arr2[j]);
+            j++;
+        }
+    }
+    return arr;
+}
+
+// Testing MergeSortArr
+var arr = MergeSortArr([1,2,2,2,7], [2,2,6,6,7]);
+console.log(`Merge Sorted Array = [${arr}]`);
